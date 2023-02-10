a56279ec90e1dc2e1711e988c76d56ef0c976e6d
Nemanja Malesija <malesija13@gmail.com>
2023-02-10 14:54:43 +0100

    Create sets from products properties

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import { productsProps } from '../constants/types';
 
 const Sidebar = ({ products }: productsProps) => {
-  return <div>Sidebar</div>;
+  const categories = Array.from(
+    new Set(['all', ...products.map((prod) => prod.category)])
+  );
+
+  const companies = Array.from(
+    new Set(['all', ...products.map((prod) => prod.company)])
+  );
+  const colors = Array.from(
+    new Set(['all', ...products.flatMap((prod) => prod.colors)])
+  );
+
+  return (
+    <section className="section-sidebar">
+      <h2 className="Filter by"></h2>
+    </section>
+  );
 };
 
 export default Sidebar;
